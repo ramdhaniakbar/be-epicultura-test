b796ece081550c214cb6f44221711b23e93cb728
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2025-02-13 16:23:18 +0700

    feat: update sequelize installation

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 9000;
+
+// Server
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "epicultura backend test",
+  })
+})
